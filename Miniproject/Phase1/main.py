d46ffd3e076e8773f2c53596be111abf6afd8e82
kenleejr92 <kenleejr92@gmail.com>
2015-09-24 20:32:32 -0500

    Can create a stream, upload images, and view streams. Very basic though

--- Miniproject/Phase1/main.py
+++ Miniproject/Phase1/main.py
@@ -1,118 +1,99 @@
+import webapp2
 import cgi
-import urllib
 
 from google.appengine.api import users
+from google.appengine.ext import blobstore
 from google.appengine.ext import ndb
-from google.appengine.api import images
+from google.appengine.ext.webapp import blobstore_handlers
+from google.appengine.ext.webapp.util import run_wsgi_app
 
-import webapp2
+# A custom datastore model for associating users with uploaded files.
 
-MAIN_PAGE_FOOTER_TEMPLATE = """\
-    <form action="/sign?%s" method="post">
-      <div><textarea name="content" rows="3" cols="60"></textarea></div>
-      <div><input type="submit" value="Sign Guestbook"></div>
-    </form>
-    <hr>
-    <form>Guestbook name:
-      <input value="%s" name="guestbook_name">
-      <input type="submit" value="switch">
-    </form>
-    <a href="%s">%s</a>
-  </body>
-</html>
-"""
+class Stream(ndb.Model):
+    name = ndb.StringProperty()
+    photos = ndb.BlobKeyProperty(repeated=True)
+    views = ndb.IntegerProperty()
 
-DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
-
-class Author(ndb.Model):
-    """Sub model for representing an author."""
-    identity = ndb.StringProperty(indexed=False)
-    email = ndb.StringProperty(indexed=False)
-
-
-class Greeting(ndb.Model):
-    """A main model for representing an individual Guestbook entry."""
-    author = ndb.StructuredProperty(Author)
-    content = ndb.StringProperty(indexed=False)
-    avatar = ndb.BlobProperty()
-    date = ndb.DateTimeProperty(auto_now_add=True)
-
-
-def guestbook_key(guestbook_name=None):
-    """Constructs a Datastore key for a Guestbook entity with guestbook_name."""
-    return ndb.Key('Guestbook', guestbook_name or 'default_guestbook')
-
-
-class MainPage(webapp2.RequestHandler):
+class MainPageHandler(webapp2.RequestHandler):
     def get(self):
         self.response.out.write('<html><body>')
-        guestbook_name = self.request.get('guestbook_name')
+        #Get the list of streams
+        qry = Stream.query()
+        #Print the names in a list
+        self.response.out.write('<ul>')
+        for stream in qry :
+            self.response.out.write('<li><a href="/view_stream/?stream_name=%s" >%s</a></li>' % (stream.name, stream.name))
+        self.response.out.write('</ul>')
+        self.response.out.write('<form action="/create_stream" method="post" >')
+        self.response.out.write('''Create Stream: <input type="text" name="stream_name" value="Name"><br><input type="submit"
+            name="submit" value="Submit"> </form></body></html>''')
 
-        greetings = Greeting.query(
-            ancestor=guestbook_key(guestbook_name)) \
-            .order(-Greeting.date) \
-            .fetch(10)
-
-        for greeting in greetings:
-            if greeting.author:
-                self.response.out.write(
-                    '<b>%s</b> wrote:' % greeting.author)
-            else:
-                self.response.out.write('An anonymous person wrote:')
-            self.response.out.write('<div><img src="/img?img_id=%s"></img>' %
-                                    greeting.key.urlsafe())
-            self.response.out.write('<blockquote>%s</blockquote></div>' %
-                                    cgi.escape(greeting.content))
-
-        self.response.out.write("""
-              <form action="/sign?%s" enctype="multipart/form-data" method="post">
-                <div><textarea name="content" rows="3" cols="60"></textarea></div>
-                <div><label>Avatar:</label></div>
-                <div><input type="file" name="img"/></div>
-                <div><input type="submit" value="Sign Guestbook"></div>
-              </form>
-              <hr>
-              <form>Guestbook name: <input value="%s" name="guestbook_name">
-              <input type="submit" value="switch"></form>
-            </body>
-          </html>""" % (urllib.urlencode({'guestbook_name': guestbook_name}),
-                        cgi.escape(guestbook_name)))
-
-
-class Image(webapp2.RequestHandler):
+#/view_stream
+class ViewStreamHandler(blobstore_handlers.BlobstoreDownloadHandler):
     def get(self):
-        greeting_key = ndb.Key(urlsafe=self.request.get('img_id'))
-        greeting = greeting_key.get()
-        if greeting.avatar:
-            self.response.headers['Content-Type'] = 'image/png'
-            self.response.out.write(greeting.avatar)
-        else:
-            self.response.out.write('No image')
+        stream_name = cgi.escape(self.request.get('stream_name'))
+        stream_key = ndb.Key(Stream,stream_name)
+        stream = stream_key.get()
+        for photo in stream.photos:
+            if not blobstore.get(photo):
+                self.error(404)
+            else:
+                self.send_blob(photo)
+
+#/view_stream
+# class UploadMoreToStreamHandler(webapp2.RequestHandler):
+#     pass
 
 
-class Guestbook(webapp2.RequestHandler):
+#/create_stream
+class CreateStreamHandler(webapp2.RequestHandler):
     def post(self):
-        guestbook_name = self.request.get('guestbook_name')
-        greeting = Greeting(parent=guestbook_key(guestbook_name))
+        # #Get the stream's name from the form
+        stream_name = cgi.escape(self.request.get('stream_name'))
+        #
+        # #Create a new stream with name, no photos, and no views initially
+        # #Add stream to the datastore
+        new_stream = Stream(name=stream_name,photos=[],views=0)
+        new_stream.key = ndb.Key(Stream, stream_name)
+        new_stream.put()
 
-        if users.get_current_user():
-            greeting.author = users.get_current_user().nickname()
+        upload_url = blobstore.create_upload_url('/upload_photo')
+        # The method must be "POST" and enctype must be set to "multipart/form-data".
+        self.response.out.write('<html><body>')
+        self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url )
+        self.response.out.write('''Upload File: <input type="file" name="file"><br> <input type="submit"
+            name="submit" value="Submit to %s"> </form></body></html>''' % stream_name)
 
-        greeting.content = self.request.get('content')
+#/UploadPhoto
+class PhotoUploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+    def post(self):
+        try:
+            upload = self.get_uploads()[0]
+            str = cgi.escape(self.request.get('submit'))
+            stream_name = str.split()[2]
+            blob_key = upload.key()
+            stream_key = ndb.Key(Stream,stream_name)
+            stream = stream_key.get()
+            stream.photos.append(blob_key)
+            stream.put()
 
-        # Get image data
-        avatar = self.request.get('img')
-        # Transform the image
-        avatar = images.resize(avatar, 32, 32)
-        greeting.avatar = avatar
+            self.redirect('/view_photo/%s' % upload.key())
 
-        greeting.put()
+        except:
+            self.error(500)
 
-        self.redirect('/?' + urllib.urlencode(
-            {'guestbook_name': guestbook_name}))
+#/view_photo
+class ViewPhotoHandler(blobstore_handlers.BlobstoreDownloadHandler):
+    def get(self, photo_key):
+        if not blobstore.get(photo_key):
+            self.error(404)
+        else:
+            self.send_blob(photo_key)
 
 
-app = webapp2.WSGIApplication([('/', MainPage),
-                               ('/img', Image),
-                               ('/sign', Guestbook)],
-                              debug=True)
+app = webapp2.WSGIApplication([('/', MainPageHandler),
+                               ('/create_stream', CreateStreamHandler),
+                               ('/upload_photo', PhotoUploadHandler),
+                               ('/view_photo/([^/]+)?', ViewPhotoHandler),
+                               ('/view_stream/.*', ViewStreamHandler)
+                              ], debug=True)
